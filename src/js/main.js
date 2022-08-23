54bdd5ff117d81ed166a7b14b64970084725e4c5
Igor <newlifeamf@gmail.com>
2022-08-23 14:24:10 +0300

    ...

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 // Custom scripts
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 700 ||
+    document.documentElement.scrollTop > 700
+  ) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
